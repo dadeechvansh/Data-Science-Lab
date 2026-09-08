c804317bf7764d59a525e1f174a29e85cc3ea512
dadeechvansh <dadeechvansh@gmail.com>
2026-09-08 11:13:18 +0530

    Third Commit

--- 08-09-2026.py
+++ 08-09-2026.py
@@ -1,9 +1,15 @@
 import pandas as pd
+import numpy as np
 
-data={
-    "Name":["Vansh","Rajyavardhan","Sunandan"],
-    "Roll_No":[30,39,15]
-
+data = {
+    "Name": ["Raj", "Aman", "Riya", "Arjun",],
+    "Age": [20, 21, 19, 22],
+    "Marks": [85, 90, 88, 92]
 }
+
 df=pd.DataFrame(data)
+print("Original Dataset:")
 print(df)
+
+features = ["Age", "Marks"]
+
